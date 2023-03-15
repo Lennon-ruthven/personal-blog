a91d079478b3094d570b5619366311feac5ce771
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-15 22:14:25 +0000

    Add files via upload

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/about', title: 'About' },
   { href: '/posts', title: 'Posts' },
-  { href: '/projects', title: 'Projects' },
   {
     type: 'dropdown',
     title: 'Other',
