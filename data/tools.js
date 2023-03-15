9228b0f31717e3ee165645e7d4fea6cb1b54b5b0
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-15 22:26:31 +0000

    Add files via upload

--- data/tools.js
+++ data/tools.js
@@ -7,5 +7,13 @@ const Tools = [
     name: `Discord`,
     description: `A voice chat app.`,
   },
+  {
+    id: `obsidian`,
+    category: [`mac`, `ios`, `web`],
+    link: `https://obsidian.md`,
+    labels: [`Knowledge Management`],
+    name: `Obsidian`,
+    description: `A note-taking app.`,
+  },
 ]
 export default Tools
