d241afd5d7895c083cffe26fc9c9b4c330892669
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-16 22:02:53 +0000

    Add files via upload

--- components/ToolsCard.js
+++ components/ToolsCard.js
@@ -9,7 +9,7 @@ const ToolsCard = ({ name, description, link, id, labels }) => {
       key={id}
       className="group bg-day relative h-full transform rounded-lg transition duration-500 hover:scale-105 "
     >
-      <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-primary-600 to-primary-500 opacity-10 blur transition duration-1000 group-hover:opacity-75 group-hover:duration-200"></div>
+      <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-lime-600 to-lime-500 opacity-10 blur transition duration-1000 group-hover:opacity-75 group-hover:duration-200"></div>
       <a className="card-c relative grid h-full  grid-cols-[86px,_1fr] content-center justify-items-start rounded-lg bg-darkSecondary  pl-10 md:pl-2">
         <div className="group relative rounded-lg px-2 py-4">
           <Link href={link}>
@@ -34,7 +34,7 @@ const ToolsCard = ({ name, description, link, id, labels }) => {
           <span className="my-2 inline-flex w-full items-center justify-between">
             <span className="inline-block rounded border border-gray-700 px-2 py-1 text-xs font-medium">
               {labels.map((tag) => (
-                <a key={tag} className="mr-3 text-xs font-medium uppercase hover:text-primary-600 ">
+                <a key={tag} className="mr-3 text-xs font-medium uppercase hover:text-lime-600 ">
                   {tag.split(' ').join('-')}
                 </a>
               ))}
