756e2615350515a505eda1dfe7d588b1e2753df1
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-16 14:57:44 +0000

    Add files via upload

--- components/Footer.js
+++ components/Footer.js
@@ -6,11 +6,6 @@ export default function Footer() {
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
-        <div className="mb-3 flex space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} />
-          <SocialIcon kind="github" href={siteMetadata.github} />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} />
-        </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
