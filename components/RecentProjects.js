c5bd907b4d423fbc33907ba418d8e7e892e5cd82
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-16 15:57:16 +0000

    Add files via upload

--- components/RecentProjects.js
+++ components/RecentProjects.js
@@ -12,7 +12,7 @@ const RecentProjects = ({ MAX_PROJECTS }) => {
     <>
       <div className="divide-y divide-gray-700">
         <div className="my-4">
-          <span className="font-poppins title-font text-3xl font-bold">Resources</span>
+          <span className="font-poppins title-font text-3xl font-bold">Recent Resources</span>
         </div>
         <div className="py-5">
           <AnimatedDiv
