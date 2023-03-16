ea748913a96d9593911c1c78a4e6a1894abe6312
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-16 13:09:32 +0000

    Add files via upload

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,7 +3,6 @@ const headerNavLinks = [
   { href: '/articles', title: 'Articles' },
   { href: '/articles', title: 'Resources' },
   { href: '/about', title: 'About' },
-  },
 ]
 
 export default headerNavLinks
