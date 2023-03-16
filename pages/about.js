35866e6fff9d7524a20dc92d671d9468005ddd7f
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-16 17:47:45 +0000

    Add files via upload

--- pages/about.js
+++ pages/about.js
@@ -13,9 +13,9 @@ function SocialLink({ className, href, children, icon: Icon }) {
     <li className="flex">
       <Link
         href={href}
-        className="group flex text-sm font-medium text-zinc-800 transition hover:text-primary-500 dark:text-zinc-200 dark:hover:text-primary-500"
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-lime-500 dark:text-zinc-200 dark:hover:text-lime-500"
       >
-        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-primary-500" />
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-lime-500" />
         <span className="ml-4">{children}</span>
       </Link>
     </li>
