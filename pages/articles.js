9770c233a61b13478753c3c46f9939ef6cdb37fb
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-17 23:05:08 +0000

    Add files via upload

--- pages/articles.js
+++ pages/articles.js
@@ -20,7 +20,7 @@ export async function getStaticProps() {
 export default function Blog({ posts, initialDisplayPosts, pagination }) {
   return (
     <>
-      <PageSEO title={`Blog - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <PageSEO title={`Articles - ${siteMetadata.author}`} description={siteMetadata.description} />
       <ListLayout
         posts={posts}
         initialDisplayPosts={initialDisplayPosts}
