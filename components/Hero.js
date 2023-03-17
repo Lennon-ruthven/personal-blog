d5ac227ea2a8971c8006cae47e881d86b2054d31
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-17 22:57:59 +0000

    Add files via upload

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,8 @@
 import Link from 'next/link'
 import { useState } from 'react'
-import { IoLogoGithub, IoLogoLinkedin, IoMail, IoCall } from 'react-icons/io5'
+import { IoLogoGithub, IoLogoLinkedin, IoMail, IoCall, IoLogoTwitter } from 'react-icons/io5'
 import Notification from './Notification'
+import React from 'react'
 
 function SocialLink({ icon: Icon, ...props }) {
   return (
