981b7e3563c12b71828a4f7a521ed1dc3dbc2239
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-17 23:57:58 +0000

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -23,8 +23,7 @@ export default function Home({ posts }) {
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <Hero />
-      <Skills />
-      <RecentProjects MAX_PROJECTS="4" />
+      <RecentProjects MAX_PROJECTS="1" />
 
       <div className="my-2 grid items-start gap-8">
                 <div className="group relative">
